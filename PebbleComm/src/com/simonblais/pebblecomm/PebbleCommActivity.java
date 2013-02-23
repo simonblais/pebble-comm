3bc364bd9903bb4e6526acbe4f4ab91a4be9d41c
Simon Blais <simon.blais@gmail.com>
2013-02-23 18:55:49 -0500

    Added test text field and button The test button sends a notification to the pebble by broadcasting an Intent

--- PebbleComm/src/com/simonblais/pebblecomm/PebbleCommActivity.java
+++ PebbleComm/src/com/simonblais/pebblecomm/PebbleCommActivity.java
@@ -1,7 +1,16 @@
 package com.simonblais.pebblecomm;
 
+import java.util.HashMap;
+import java.util.Map;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 
 public class PebbleCommActivity extends Activity
 {
@@ -11,5 +20,34 @@ public class PebbleCommActivity extends Activity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+    }
+    
+    
+    /** Called when the send button is pressed */
+    public void sendNotification(View view) {
+    	sendNotificationToPebble("Test Title", "Test Body");
+    }
+    
+    
+    
+    /** Utility method to send an alert notification to Pebble */
+    public void sendNotificationToPebble(String title, String body) {
+    	
+    	final Intent i = new Intent("com.getpebble.action.SEND_NOTIFICATION");
+    	
+    	final Map<String, Object> data = new HashMap<String, Object>();
+    	
+    	data.put("title", title);
+    	data.put("body", body);
+    	
+    	final JSONObject jsonData = new JSONObject(data);
+    	final String notificationData = new JSONArray().put(jsonData).toString();
+    	
+    	i.putExtra("messageType", "PEBBLE_ALERT");
+    	i.putExtra("sender", "PebbleCommunicator");
+    	i.putExtra("notificationData", notificationData);
+    	
+    	Log.d( "PebbleComm", "Sending notification to Pebble" + notificationData);
+    	sendBroadcast(i);
     }
 }
