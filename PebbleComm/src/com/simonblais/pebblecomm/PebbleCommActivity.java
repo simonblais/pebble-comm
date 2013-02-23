0bde1be07f62ad368442a267232bad597cfc0400
Simon Blais <simon.blais@gmail.com>
2013-02-23 15:33:51 -0500

    Initial commit

---
+++ PebbleComm/src/com/simonblais/pebblecomm/PebbleCommActivity.java
@@ -0,0 +1,15 @@
+package com.simonblais.pebblecomm;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class PebbleCommActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
